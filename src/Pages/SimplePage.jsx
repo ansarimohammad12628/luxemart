99344941a9b02a7e237e2d0e41cbdaa4ba9e574e
ansarimohammad12628 <ansarimohammad12628@gmail.com>
2025-10-22 18:01:36 +0530

    update simplepage

--- src/Pages/SimplePage.jsx
+++ src/Pages/SimplePage.jsx
@@ -9,7 +9,7 @@ const SimplePage = () => {
       <div class="intro-container">
     <h1>Welcome to <span>LuxeMart</span></h1>
     <p>Discover your perfect makeup look</p>
-    <a href="home" class="btn">Explore Now</a>
+    <a href="login" class="btn">Explore Now</a>
   </div>
     </div>
   )

c4cb8a77d23ed445f7a3353a9b434e0290aebd95
ansarimohammad12628 <ansarimohammad12628@gmail.com>
2025-10-23 12:46:32 +0530

    vite update

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/luxemart/'  // 👈 your repo name
+  base: '/luxemart'  // 👈 your repo name
 })
